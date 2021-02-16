dcd6accf313ed10a8228b2d4e05a9b9af66b88a6
VladyslavSt <vladyslav.stankevych@ukrainiansoftware.com>
2021-02-16 16:08:18 +0200

    Added user information to the Activity log

--- lib/coinpayments-api-handler.php
+++ lib/coinpayments-api-handler.php
@@ -160,7 +160,7 @@ class Coinpayments_API_Handler
      * @return bool|mixed
      * @throws Exception
      */
-    public function create_invoice($invoice_id, $currency_id, $amount, $display_value)
+    public function create_invoice($invoice_params)
     {
 
         if ($this->webhooks) {
@@ -169,18 +169,51 @@ class Coinpayments_API_Handler
             $action = self::API_SIMPLE_INVOICE_ACTION;
         }
 
+
         $params = array(
             'clientId' => $this->client_id,
-            'invoiceId' => $invoice_id,
+            'invoiceId' => $invoice_params['invoice_id'],
             'amount' => [
-                'currencyId' => $currency_id,
-                "displayValue" => $display_value,
-                'value' => $amount
+                'currencyId' => $invoice_params['currency_id'],
+                "displayValue" => $invoice_params['display_value'],
+                'value' => $invoice_params['amount']
             ],
         );
 
+        $params = $this->append_billing_data($params, $invoice_params['billing_data']);
         $params = $this->append_invoice_metadata($params);
         return $this->send_request('POST', $action, $this->client_id, $params, $this->client_secret);
+    }
+
+    /**
+     * @array $billing_data
+     * @return mixed
+     */
+    protected function append_billing_data($request_params, $billing_data)
+    {
+        $request_params['buyer'] = array(
+            'companyName' => $billing_data['company'],
+            'name' => array(
+                'firstName' => $billing_data['first_name'],
+                'lastName' => $billing_data['last_name']
+            ),
+            'emailAddress' => $billing_data['email'],
+        );
+
+        if (!empty($billing_data['address']) &&
+            !empty($billing_data['city']) &&
+            preg_match('/^([A-Z]{2})$/', $billing_data['country']))
+        {
+            $request_params['buyer']['address'] = array(
+                'address1' => $billing_data['address']['address_1'],
+                'address2' => $billing_data['address']['address_2'],
+                'provinceOrState' => $billing_data['address']['state'],
+                'city' => $billing_data['address']['city'],
+                'countryCode' => $billing_data['address']['country'],
+                'postalCode' => $billing_data['address']['postcode']
+            );
+        }
+        return $request_params;
     }
 
     /**
